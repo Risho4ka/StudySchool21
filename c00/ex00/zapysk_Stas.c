c243a8d11adb65f4621c91f3620cb454fa64c5ea
rlyAtas <stas.s.shevchenko@gmail.com>
2022-01-28 17:55:37 +0700

    There are comments to exercice c00/ex00

---
+++ c00/ex00/zapysk_Stas.c
@@ -0,0 +1,22 @@
+#include "ft_putchar.c"
+
+int main(void) {
+	ft_putchar('$');
+	ft_putchar('\n');
+return(0);
+}
+
+/*
+ * для лучшей читабельньости лучше оформить так
+ * сам файл лучше назвать main.c так сразу будет понятно, что внутри главная программа
+ */
+
+#include "ft_putchar.c"
+
+int main(void) {
+	
+	ft_putchar('$');
+	ft_putchar('\n');
+	
+	return(0);
+}
