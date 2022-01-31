188755a765c8e57ba7641ffe8f9ac6bf91312662
rlyAtas <stas.s.shevchenko@gmail.com>
2022-01-31 16:59:14 +0700

    Исправил ошибку с повтором чисел

--- c00/ex05/variant_Stas.c
+++ c00/ex05/variant_Stas.c
@@ -1,7 +1,30 @@
-#include <unistd.h> 
+#include <unistd.h>
+#include <stdio.h>
+
+void ft_print_comb_Stas( int );
 
 int main( void ) {
 
+	ft_print_comb_Stas( 3 );
+
+	return(1);
+}
+
+void ft_print_comb_Stas( int n ) {
+
+
+	/*int out[n];
+	int max = 10;
+
+	if( n == 0 )
+		return;
+		
+
+	for( int i=2; i<=n; i++)
+		max *= 10; 
+
+	printf("%d",max);*/
+	
 	int i = 11;
 
 	while( ++i <= 786 ) {
@@ -10,7 +33,7 @@ int main( void ) {
 		char num2 = (char) 48+(int)(i-100*((int)i/100))/10;
 		char num1 = (char) 48+i%10;
 
-		if( num1 == num2 || num2 == num3 || num1 == num3)
+		if( num1 <= num2 || num2 <= num3 || num1 <= num3 )
 			continue;
 
 		write(1, &num3, 1);
@@ -19,6 +42,4 @@ int main( void ) {
 		write(1, ", ", 2);
 	}
 	write(1, "789", 3);
-
-	return(1);
 }
