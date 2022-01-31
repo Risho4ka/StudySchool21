b2fa0fe5c12412aeaec42af13a1087e57907e076
Risho4ka <fomina.tvn@gmail.com>
2022-01-31 18:49:24 +0700

    Exercise 06 ready

---
+++ c00/ex06/ft_print_comb2.c
@@ -0,0 +1,31 @@
+#include <unistd.h> 
+
+void ft_print_comb2(void) {
+
+    char nums[10] = "0123456789";
+    int i;
+    int j;
+
+        i = 0;
+        while ( i < 99) {
+
+            j = i + 1;
+
+            while ( j <100) {
+
+                write(1, &nums[ (int)i/10 ], 1);
+                write(1, &nums[i%10], 1);
+                write(1, " ",  1);
+                write(1, &nums[ (int)j/10 ], 1);
+                write(1, &nums[j%10], 1);
+
+                    if( !(i == 98 && j == 99) ) 
+                
+                    write(1, ", ", 2);
+
+                j++;
+            }
+            
+            i++;
+        }
+}
