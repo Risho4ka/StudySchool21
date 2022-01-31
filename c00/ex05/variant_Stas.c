e2a7df25e974a6e11b4f519dd0d40b419737a006
rlyAtas <stas.s.shevchenko@gmail.com>
2022-01-31 17:12:42 +0700

    Отшлифовал решение

--- c00/ex05/variant_Stas.c
+++ c00/ex05/variant_Stas.c
@@ -1,5 +1,4 @@
 #include <unistd.h>
-#include <stdio.h>
 
 void ft_print_comb_Stas( int );
 
@@ -12,36 +11,21 @@ int main( void ) {
 
 void ft_print_comb_Stas( int n ) {
 
-	/* int out[n];
-	int max = 10;
-	int num = 0;
-
-	if( n == 0 )
-		return;
-		
-	while( num < max ) {
-
-
-	}
-
-
-	printf("%d",max);*/
-	
 	int i = 11;
 
-	while( ++i <= 786 ) {
+	while( ++i <= 689 ) {
 
 		char num3 = (char) 48+(int)i/100;
 		char num2 = (char) 48+(int)(i-100*((int)i/100))/10;
 		char num1 = (char) 48+i%10;
 
-		if( num1 <= num2 || num2 <= num3 || num1 <= num3 )
-			continue;
+		if( num3 < num2 && num2 < num1 ) {
 
-		write(1, &num3, 1);
-		write(1, &num2, 1);
-		write(1, &num1, 1);
-		write(1, ", ", 2);
+			write(1, &num3, 1);
+			write(1, &num2, 1);
+			write(1, &num1, 1);
+			write(1, ", ", 2);
+		}
 	}
 	write(1, "789", 3);
 }
