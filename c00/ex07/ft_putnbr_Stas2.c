54f3983af30c8cf59f568326116226205ce256a7
rlyAtas <stas.s.shevchenko@gmail.com>
2022-02-02 18:41:13 +0700

    исправлены ошибки, улучшено решение с рекурсией

--- c00/ex07/ft_putnbr_Stas2.c
+++ c00/ex07/ft_putnbr_Stas2.c
@@ -1,65 +1,32 @@
 #include <unistd.h>
 #include <stdio.h>
 
-
-// решение 1, через цикл
-/*int main() {
-
-	int x = 22333;
-	int divide = 1000000000;
-	int num;
-	int zeros = 1;
-	char c;
-
-	if( x<0 ) {
-		write(1,"-",1);
-		x = -x;
-	}
-
-	while( divide>=1 ) {
-
-		num = (int)x/divide;
-		c = (char)num+48;
-
-		if( num!= 0 )
-			zeros = 0;
-		
-		if( zeros == 0 ) 
-			write(1,&c,1);
-		
-		x -= num*divide;
-		
-		divide /= 10;
-	}
-	
-	return(0);
-}*/
-
-// решение 2, через рекурсию
+// решение через рекурсию
 void numbers( int );
+void ft_putchar( char );
 
 int main() {
 
-	int x = 34567;
-
-	if( x<0 ) {
-		write(1,"-",1);
-		x *= -1;
-	}
+	//int x = -2147483648;
+	//int x = 2147483647;
+	int x = 0;
+	
 	numbers( x );
 }
 
 void numbers( int x ) {
 
-	int divide = 1;
-	while( divide<x/10 )
-		divide *= 10;
+	if( x>=10 || x<=-10 )
+		numbers( (int) x/10 );
 
-	int num = (int) x/divide;
-	char c = (char) num+48;
+	if( x>-10 && x<0 )
+		ft_putchar('-');
 
-	write(1,&c,1);
+	x = x<0 ? -(x%10) : x%10;
 
-	if( divide/10>=1 ) 
-		numbers(x-num*divide);
+	ft_putchar((char)(x+48));
+}
+
+void ft_putchar( char c ) {
+	write(1, &c, 1);
 }
