98ec7024a65e58dfa34fce1cb3b6b8e054cbf733
Risho4ka <fomina.tvn@gmail.com>
2022-02-04 14:34:44 +0700

    Exercise 03 for c01  ready

---
+++ c01/ex03/main.c
@@ -0,0 +1,15 @@
+#include "ft_div_mod.c"
+#include <stdio.h>
+
+int main(void) {
+
+	int div ;
+	int mod ;
+
+	ft_div_mod(6, 4, &div, &mod); // передали адрес переменной
+
+	printf("%d%c%d\n", div, ' ', mod);
+
+	return(0);
+}
+
