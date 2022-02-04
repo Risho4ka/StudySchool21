aa726094f9946369f8964e0bb6a426df90462905
Risho4ka <fomina.tvn@gmail.com>
2022-02-04 14:27:25 +0700

    Exercise 01 for c01  ready

---
+++ c01/ex01/main.c
@@ -0,0 +1,36 @@
+#include "ft_ultimate_ft.c"
+#include <stdio.h>
+
+int main(void) {
+
+	int x = 1;
+
+	int *a;
+	int **b;
+	int ***c;
+	int ****d;
+	int *****e;
+	int ******f;
+	int *******g;
+	int ********h;
+	int *********j;
+	
+	a = &x;
+	b = &a;
+	c = &b;
+	d = &c;
+	e = &d;
+	f = &e;
+	g = &f;
+	h = &g;
+	j = &h;
+
+	printf("%d\n", x);
+
+	ft_ultimate_ft(j); // передали адрес переменной
+
+	printf("%d\n", x);
+
+	return(0);
+}
+
