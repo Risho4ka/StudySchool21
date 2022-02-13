77bdd49713c7762fedb7360a8083e996b0577c30
Risho4ka <fomina.tvn@gmail.com>
2022-02-13 14:12:27 +0700

    Changes file comments ex07

--- c02/ex09/ft_strlcapitalize.c
+++ c02/ex09/ft_strlcapitalize.c
@@ -1,19 +1,22 @@
 char *ft_strlcapitalize(char *str) {
 
 	int j = 0;
+	// смещение между большими и маленькими английскими символами 
+	char delta = (char)32;
 
-	while (str[j] != '\0') {
+	//преобразуем все буквы в маленькие
+	while (str[j]) {
 
-		if ( str[j] >= (char)65 &&  str[j] <= (char)95 )
-			str[j] = (str[j] + (char)32);
+		if ( str[j] >= 'A' &&  str[j] <= 'Z' )
+			str[j] = str[j] + delta;
 			
 		j++;
 	}
 
 	int i = 0;
 
-	if ( str[0] >= 'a' && str[0] <= 'z' ) // проверим первую букву
-		str[0] = (str[0] - 32);
+	if ( str[0] >= 'a' && str[0] <= 'z' ) // проверим первую букву, так как перед ней нет никакого символа
+		str[0] = str[0] - delta;
 
 	while (str[i] != '\0') {
 
@@ -22,7 +25,7 @@ char *ft_strlcapitalize(char *str) {
 			!(str[i] >= 'A' && str[i] <= 'Z') && // и это не большая буква
 			!(str[i] >= '0' && str[i] <= '9') && // и это не цифра
 			str[i + 1] >= 'a' && str[i + 1] <= 'z' ) // следующий символ буква
-		 	str[i + 1] = (str[i + 1] - 32);
+		 	str[i + 1] = (str[i + 1] - delta);
 			
 		i++;
 	}
